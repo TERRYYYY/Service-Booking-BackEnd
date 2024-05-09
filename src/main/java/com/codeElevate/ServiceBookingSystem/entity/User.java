49e2ffbcd18aec7db32f83c572cfef38c058f7bc
TERRYYYY <kariuki.terry11@gmail.com>
2024-05-09 18:30:31 +0300

    Creating User Entity

---
+++ src/main/java/com/codeElevate/ServiceBookingSystem/entity/User.java
@@ -0,0 +1,35 @@
+package com.codeElevate.ServiceBookingSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+//Entity annotation -> Signals the JPA provider to tell them that the class should be treated as a table in a database
+@Entity
+@Table(name = "users")
+
+//Data annotation -> Shortcut to get  the Getter and Setter
+@Data
+
+public class User {
+//    Define the properties needed
+
+    //    @Id annotation -> Used to indicate the primary key of an entity
+    @Id
+
+    // @GeneratedValue -> It will use auto increment method in the Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String lastname;
+
+    private String phone;
+
+    private UserRole role;
+
+}
